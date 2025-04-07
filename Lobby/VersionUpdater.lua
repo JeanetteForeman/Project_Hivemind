303b7ee6d14bc1562f9f9bc928dd6c810636aead
Ryan Rodriguez <tile124@icloud.com>
2025-04-06 22:15:26 -0400

    Updated plane cutscene

---
+++ Lobby/VersionUpdater.lua
@@ -0,0 +1,23 @@
+-- Updates the publish # for the version GUI
+-- For use only in Alpha stage of game
+-- @RyanRodrii (tile127)
+-- 02/06/2025
+
+-- CONFIG
+local VERSIONGUI_ENABLED = true
+
+-- REFERENCES
+local versionLabel = script.Parent
+local versionFrame = versionLabel.Parent
+local versionGUI = versionFrame.Parent
+
+-- CODE
+if VERSIONGUI_ENABLED then
+	versionGUI.Enabled = true
+	versionFrame.Visible = true
+	versionLabel.Text = "Hivemind Lobby #" .. game.PlaceVersion
+else
+	versionGUI.Enabled = false
+	versionFrame.Visible = false
+end
+
